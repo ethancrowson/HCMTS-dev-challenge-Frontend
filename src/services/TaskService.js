2c77e92a33ae5d439ae3a88613b0458877738e36
Ethan Crowson <ethancrowson@hotmail.co.uk>
2025-04-16 22:13:57 +0100

    Creation of createTask function.

--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -4,3 +4,11 @@ export async function getTasks() {
     const response = await fetch(BASE_URL);
     return response.json();
 }
+export async function createTask(task) {
+    const response = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(task)
+    });
+    return response.json();
+}
